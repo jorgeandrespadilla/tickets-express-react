a4d3c17a055e723b881674d38b1130de23911964
Luis Corales <lcorales@stackbuilders.com>
2022-07-02 18:59:20 -0500

    Filter people by its ticket expiration date

--- server/filters.js
+++ server/filters.js
@@ -1,4 +1,22 @@
 // Filter by date and till today
-export const filterByDate = (people, date) => {
-  return date ? { id: 2, name: "Daniela" } : { id: 3, name: "Jose" };
+export const filterExpirationByDate = (people, date) => {
+  return people
+    .map((item) => {
+      let monthsToAdd =
+        item.type === "mensual" ? 1 : item.type === "semestral" ? 6 : 12;
+      return {
+        id: item.id,
+        name: item.name,
+        buy_date: item.buy_date,
+        type: item.type,
+        expiry_date: new Date(
+          new Date(item.buy_date).setMonth(
+            new Date(item.buy_date).getMonth() + monthsToAdd
+          )
+        ),
+      };
+    })
+    .filter((item) => {
+      return item.expiry_date <= new Date() && item.expiry_date >= date;
+    });
 };
--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import path, { join } from "path";
 import { fileURLToPath } from "url";
 
 import { getPeople } from "./data/people.js";
-import { filterByDate } from "./filters.js";
+import { filterExpirationByDate } from "./filters.js";
 
 const port = process.env.PORT || 5100;
 
@@ -28,7 +28,7 @@ app.get("/", (req, res) => {
 
 // Retrieve all filtered persons and data by date
 app.post("/", (req, res) => {
-  return res.json(filterByDate(getPeople(), req.body.date));
+  return res.json(filterExpirationByDate(getPeople(), new Date(req.body.date)));
 });
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
